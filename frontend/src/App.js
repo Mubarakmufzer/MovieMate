ce624eb481a1fa95ab664b985dedeb7dbccd9193
muhammed mubarak <muhammedmubarak204@gmail.com>
2025-09-24 22:41:43 +0530

    Initial project setup with frontend and backend directories

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        MovieMate + Tailwind is Working 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
